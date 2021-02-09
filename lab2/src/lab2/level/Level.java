3a69dce3a96be6de6d8ed51b23cd733ee17d8411
Johan Jonsson <ojoojo-8@student.ltu.se>
2021-02-09 13:14:55 +0100

    Final

---
+++ lab2/src/lab2/level/Level.java
@@ -0,0 +1,256 @@
+package lab2.level;
+
+import java.util.ArrayList;
+import java.util.Observable;
+
+public class Level extends Observable {
+
+	// lista som innehåller alla rum
+	private ArrayList<Room> allRooms;
+	private int roomCount = 0;
+
+	public Level() {
+		this.allRooms = new ArrayList<Room>();
+	}
+
+	private void addRoom(Room r) {
+		roomCount++;
+		this.allRooms.add(r);
+	}
+	
+	Room getRoom(int i) {
+		return this.allRooms.get(i);
+	}
+	
+	int numberOfRooms() {
+		return this.allRooms.size();
+	}
+	
+	//Kollar om rum finns i level
+	private boolean hasRooms() {
+		
+		if (this.numberOfRooms() == 0) {
+			return false;
+		} else {
+			return true;
+		}
+		
+	}
+	
+	boolean hasFirstLocation() {
+		
+		int i = 0;
+		
+		while (i < numberOfRooms()) {
+			
+			if (this.getRoom(i).isFirstLocation() == true) {
+				return true;
+			}
+			
+			i++;
+			
+		}
+		
+		return false;
+		
+	}
+	
+	//Kollar overlap
+	private boolean overlap(Room r, int i, int x, int y) {
+		
+		int placedWidth = this.getRoom(i).getWidth();
+		int placedHeight = this.getRoom(i).getHeight();
+		int placedX = this.getRoom(i).getX();
+		int placedY = this.getRoom(i).getY();
+		
+		int newHeight = r.getHeight();
+		int newWidth = r.getWidth();
+		int newX = x;
+		int newY = y;
+		
+		return
+			//Ny punkt sydöst 
+			(
+			newX >= placedX
+			&&
+			newX < placedX + placedWidth
+			&&
+			newY >= placedY
+			&&
+			newY < placedY + placedHeight
+			)
+			||
+			//Existerande punkt sydöst/Ny punkt nordväst
+			(
+			placedX >= newX
+			&&
+			placedX < newX + newWidth
+			&&
+			placedY >= newY
+			&&
+			placedY < newY + newHeight
+			)
+			||
+			//Ny punkt sydväst
+			(
+			newX <= placedX
+			&&
+			newY >= placedY
+			&&
+			newX + newWidth > placedX
+			&&
+			newY < placedY + placedHeight
+			)
+			||
+			//Ny punkt nordöst
+			(
+			newX >= placedX
+			&&
+			newY <= placedY
+			&&
+			newX < placedX + placedWidth
+			&&
+			newY + newHeight > placedY
+			)
+		;
+	}
+	
+	public boolean place(Room r, int x, int y) {
+		
+		//Kör överlappkoll om rum finns
+		if (this.hasRooms() == true) {
+
+			int i = 0;
+			
+			while (i < this.numberOfRooms()) {
+				
+				if (this.overlap(r, i, x, y) == true) {
+					return false;
+				}
+				
+				i++;
+
+			}
+		}
+		
+		if (this.hasFirstLocation() == true) {
+			return false;
+		} else {
+		
+			// sätter koordinater för övre vänstra hörnet av nytt rum
+			r.setX(x);
+			r.setY(y);
+			this.addRoom(r);
+			r.setId(roomCount);
+			return true;
+			
+		}
+	}
+	
+//tilldelar ett rum ursprunglig spelare
+	public boolean firstLocation(Room r) {
+		
+		int i = 0;
+		
+		while (i < this.numberOfRooms()) {
+			
+			if (this.getRoom(i).isFirstLocation() == true) {
+				return false;
+			} 
+			
+			i++;
+			
+		}
+		
+		r.setPlayerStatus(true);
+		r.setFirstLocation(true);
+		return true;
+		
+	}
+
+	Room playerLocation() {
+		
+		int i = 0;
+		
+		while (i < this.numberOfRooms()) {
+			
+			if (this.getRoom(i).getPlayerStatus() == true) {
+				break;
+			}
+			
+			i++;
+			
+		}
+		
+		return getRoom(i);
+		
+	}
+	
+	//Move player
+	void playerMoveNorth(Room r) {
+		r.setPlayerStatus(false);
+		r.northConnection().setPlayerStatus(true);
+		this.setChangedNotify();
+	}
+	
+	void playerMoveEast(Room r) {
+		r.setPlayerStatus(false);
+		r.eastConnection().setPlayerStatus(true);
+		this.setChangedNotify();
+	}
+	
+	void playerMoveSouth(Room r) {
+		r.setPlayerStatus(false);
+		r.southConnection().setPlayerStatus(true);
+		this.setChangedNotify();
+	}
+	
+	void playerMoveWest(Room r) {
+		r.setPlayerStatus(false);
+		r.westConnection().setPlayerStatus(true);
+		this.setChangedNotify();
+	}
+	
+	private void setChangedNotify() {
+		setChanged();
+		notifyObservers();
+	}
+	
+	public void printPlayerLocation() {
+	 		
+	 		String playerRoomS = "";
+	 		
+	 		for (int i = 0; i < this.numberOfRooms(); i++) {
+	 			if (this.getRoom(i).getPlayerStatus() == true ) {
+		 			playerRoomS +=
+						"Player in room " + this.getRoom(i).getId() + "\n";
+	 			} else {
+	 				continue;
+	 			}
+	 		}
+	 		
+	 		System.out.println(playerRoomS);
+	 	}
+	
+	private String roomPositions() {
+		
+		String coordinatesById = "";
+		
+		for (int i = 0; i < this.numberOfRooms(); i++) {
+			coordinatesById +=
+			"iD = " + this.getRoom(i).getId() + "\n" +
+			"X = " + this.getRoom(i).getX() + "\n" +
+			"Y = " + this.getRoom(i).getY() + "\n" + "\n";
+		}
+		
+		return coordinatesById;
+		
+	}
+	
+	public String toString() {
+		return 
+			this.roomPositions() +
+			"Number of rooms = " + this.numberOfRooms() + "\n";
+	}
+
+}
---
+++ lab2/src/lab2/level/Room.java
@@ -0,0 +1,173 @@
+package lab2.level;
+
+import java.awt.Color;
+
+public class Room {
+
+	private int posX, posY, width, height;
+	private Color color;
+	private Room northWall, eastWall, southWall, westWall;
+	private boolean hasPlayer;
+	private int id;
+	private boolean firstLocation;
+
+	public Room(int dx, int dy, Color color) {
+		
+		this.id = 0;
+		
+		this.posX = 0;
+		this.posY = 0;
+		this.width = dx;
+		this.height = dy;
+		this.color = color;
+		
+		this.northWall = null;
+		this.eastWall = null;
+		this.southWall = null;
+		this.westWall = null;
+		
+		this.firstLocation = false;
+		this.hasPlayer = false;
+		
+	}
+	
+	//Identification
+	void setId(int i) {
+		this.id = i;
+	}
+	
+	int getId() {
+		return this.id;
+	}
+	
+	Color getColor() {
+		return this.color;
+	}
+	
+	//Player status
+	boolean getPlayerStatus() {
+		return this.hasPlayer;
+	}
+	
+	void setPlayerStatus(boolean hasPlayer) {
+		this.hasPlayer = hasPlayer;
+	}
+	
+	void setFirstLocation(boolean firstLocation) {
+		this.firstLocation = firstLocation;
+	}
+	
+	boolean isFirstLocation() {
+		return this.firstLocation;
+	}
+	
+	//Get position
+	int getX() {
+		return this.posX;
+	}
+	
+	int getY() {
+		return this.posY;
+	}
+	
+	//Set position
+	void setX(int x) {
+		this.posX = x;
+	}
+	
+	void setY(int y) {
+		this.posY = y;
+	}
+	
+	//Get dimensions
+	int getHeight() {
+		return this.height;
+	}
+	
+	int getWidth() {
+		return this.width;
+	}
+	
+	//Koppla samman med existerande rum
+	public void connectNorthTo(Room r, Level lv) { 
+		if (lv.hasFirstLocation() == false) {
+			this.northWall = r;
+		}
+	}
+
+	public void connectEastTo(Room r, Level lv) {
+		if (lv.hasFirstLocation() == false) {
+			this.eastWall = r;
+		}
+	}
+
+	public void connectSouthTo(Room r, Level lv) {
+		if (lv.hasFirstLocation() == false) {
+			this.southWall = r;
+		}
+	}
+
+	public void connectWestTo(Room r, Level lv) {
+		if (lv.hasFirstLocation() == false) {
+			this.westWall = r;
+		}
+	}
+		
+	//vilket rum vid vägg väderstreck?
+	public Room northConnection() {
+		return this.northWall;
+	}
+	
+	public Room eastConnection() {
+		return this.eastWall;
+	}
+	
+	public Room southConnection() {
+		return this.southWall;
+	}
+	
+	public Room westConnection() {
+		return this.westWall;
+	}
+	
+	private String printIsFirstLocation() {
+		
+		if (this.isFirstLocation() == true) {
+			return "Is first location";
+		} else {
+			return "";
+		}
+		
+	}
+	
+	private String connectsTo() {
+		
+		String connections = "";
+		
+		if (this.northWall != null) {
+			connections += "North wall connects to room " +  this.northWall.id + "\n";
+		}
+		if (this.eastWall != null) {
+			connections += "East wall connects to room " +  this.eastWall.id + "\n";
+		}
+		if (this.southWall != null) {
+			connections += "South wall connects to room " +  this.southWall.id + "\n";
+		}
+		if (this.westWall != null) {
+			connections += "West wall connects to room " +  this.westWall.id + "\n";
+		}
+		
+		return connections;
+	}
+	
+	public String toString() {
+		return 
+			this.printIsFirstLocation() + "\n" +
+			"iD = " + this.id + "\n" +
+			"width = " + this.width + "\n" +
+			"height = " + this.height + "\n" +
+			"Color = " + this.color + "\n" +
+			this.connectsTo();
+	}
+
+}
